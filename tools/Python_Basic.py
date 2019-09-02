825a1b19a5d8d39c001ac44a3a0586cd8f4f5f0d
AkishinoShiame <akishinoshiame@icloud.com>
2019-09-02 11:50:08 +0800

    Add multi-lang in README.MD @ CST 2019年 9月  2日 月曜日 11:50:07

---
+++ tools/Python_Basic.py
@@ -0,0 +1,23 @@
+# import <library>
+
+'''
+This file is created by @AkishinoShiame 
+On date 2019/09/02
+File Name: Python_Basic.py
+'''
+
+Num_temp = 2
+Float_temp = .7
+String_temp = "ConoHa IchiBan !"
+
+def print_the_predefine():
+    print(Num_temp, Float_temp, String_temp)
+
+def Standard_Print():
+    print("Hello Python3 !")
+    print "Hello Python2 !"
+
+if __name__ == "__main__":
+    Standard_Print()
+    print_the_predefine()
+    return 0
